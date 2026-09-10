057dc2facaa1e273238129175fe6eb472855a146
mattmedina4224-create <matt.medina4224@gmail.com>
2026-09-10 14:35:34 -0600

    Remove unused root public route shim

--- public.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-// Legacy compatibility shim. The active public routes live in routes/public.js.
-// Keeping this file free of old duplicated UI prevents stale branding or emoji
-// markup from being reintroduced accidentally.
-module.exports = require('./routes/public');
