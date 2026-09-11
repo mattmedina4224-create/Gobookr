95f06ad06fbabac061012c79e9500b4414707089
mattmedina4224-create <matt.medina4224@gmail.com>
2026-09-10 20:40:06 -0600

    Use the same ICO favicon on every GoBookr page

--- public/location.js
+++ public/location.js
@@ -1,20 +1,19 @@
 'use strict';
 
 (() => {
-  // Force the exact GoBookr favicon on every page.
-  const faviconHref = '/gobookr-favicon-final-20260910.svg';
+  // Use the same browser-tab favicon on every GoBookr page.
+  const faviconHref = '/favicon.ico?v=gobookr-tab-final-1';
   document.querySelectorAll('link[rel*="icon"]').forEach((link) => link.remove());
 
   const icon = document.createElement('link');
   icon.rel = 'icon';
-  icon.type = 'image/svg+xml';
-  icon.sizes = 'any';
+  icon.type = 'image/x-icon';
   icon.href = faviconHref;
   document.head.appendChild(icon);
 
   const shortcut = document.createElement('link');
   shortcut.rel = 'shortcut icon';
-  shortcut.type = 'image/svg+xml';
+  shortcut.type = 'image/x-icon';
   shortcut.href = faviconHref;
   document.head.appendChild(shortcut);
 })();
